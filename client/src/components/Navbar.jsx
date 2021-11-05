745dfba8982f6e622fabe4563d456887c809e8be
mrrobot <pojskicsafet@gmail.com>
2021-11-05 11:03:56 +0100

    Moved client side to client folder, and made new folder for server side

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,11 +6,9 @@ export const Navbar = ({ darkTheme, setDarkTheme }) => {
         <>
             <div className="p-3 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
                 <div className='flex justify-between items-center space-x-5 w-screen'>
-                    <Link to='/'>
-                        <p className='text-2xl bg-gray-900 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900'>
-                            GitHub Prober
-                        </p>
-                    </Link>
+                    <p className='text-2xl bg-gray-900 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900'>
+                        GitHub Prober
+                    </p>
 
                     <button type='button' onClick={() => setDarkTheme(!darkTheme)} className='text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg'>
                         {darkTheme ? 'Light 💡' : 'Dark 🌙'}
