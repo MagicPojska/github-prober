cf980d368f8975d11e875db11aa830fd0fd2b242
mrrobot <pojskicsafet@gmail.com>
2021-11-06 23:48:55 +0100

    Removed unused variables and libraries and added 2 new images

--- client/src/index.js
+++ client/src/index.js
@@ -5,16 +5,13 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import { ContextProvider } from "./context/ResultsContextProvider";
-import { CookiesProvider } from "react-cookie";
 
 ReactDOM.render(
-  <CookiesProvider>
-    <ContextProvider>
-      <Router>
-        <App />
-      </Router>
-    </ContextProvider>
-  </CookiesProvider>,
+  <ContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </ContextProvider>,
 
   document.getElementById("root")
 );
